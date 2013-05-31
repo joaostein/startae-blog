49ac8002e137dc2a921a55dc7ceb7030fa6dd449
Renato Carvalho <renatolz@gmail.com>
2013-05-30 22:37:27 -0300

    Limpando espaços config

--- config.rb
+++ config.rb
@@ -89,21 +89,21 @@ set :images_dir, 'images'
 configure :build do
   # For example, change the Compass output style for deployment
   activate :minify_css
-  
+
   # Minify Javascript on build
   activate :minify_javascript
-  
+
   # Enable cache buster
   activate :cache_buster
-  
+
   # Use relative URLs
   # activate :relative_assets
-  
+
   # Compress PNGs after build
   # First: gem install middleman-smusher
   # require "middleman-smusher"
   # activate :smusher
-  
+
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
