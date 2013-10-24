057ee01c9053e6f6e391b86fcd0b0080445b9071
Renato Carvalho <renatolz@gmail.com>
2013-10-24 11:31:15 -0200

    Atualizando summary_length
    Signed-off-by: Renato Carvalho <renatolz@gmail.com>

--- config.rb
+++ config.rb
@@ -15,7 +15,7 @@ activate :blog do |blog|
   blog.taglink = "categoria/:tag.html"
   blog.layout = "_layouts/_article"
   blog.summary_separator = /(READMORE)/
-  blog.summary_length = 144
+  blog.summary_length = 256
   blog.year_link = ":year.html"
   blog.month_link = ":year/:month.html"
   blog.day_link = ":year/:month/:day.html"
